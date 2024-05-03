75f8b5cdbd648aa9ebdf8d629959656cf3337553
Lukas Tomasek <vedstav31@gmail.com>
2024-05-03 11:32:36 -0400

    feedback

--- javascript/loader.js
+++ javascript/loader.js
@@ -8,6 +8,10 @@ const hideLoading = () => {
     loaderContainer.style.display = 'none';
 };
 
+// do not export like this, if you have multiple functions to export use object exporting, like this:
+/**
+export {displayLoading, hideLoading}
+**/
 export {displayLoading};
 export {hideLoading}
 
