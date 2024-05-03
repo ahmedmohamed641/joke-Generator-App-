18d83756618b6827652b5998b4270f5708bc04c6
Lukas Tomasek <vedstav31@gmail.com>
2024-05-03 11:31:12 -0400

    feedback
    feedback

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,23 @@
+/**
+    - FEEDBACK
+    - use proper folder naming, don't name `javascript` folder, name it `js`
+    - also you need to create folders for other files loader folder -> loader.js
+**/
+
 import { displayLoading, hideLoading } from "./loader";
 
 const btn = document.getElementById("btn")
 const jokeText = document.getElementById("joke-text")
 const loaderContainer = document.querySelector('.loader-container');
 
+// this is unnesessary, in html the main js file should load last
 window.addEventListener("load", () => {
     loaderContainer.style.display = 'none';
 });
 
 btn.addEventListener("click", jokeGenerator)
 
+// the fetch call should be helper function, in its own file
 async function jokeGenerator() {
     const endpoint = "https://api.chucknorris.io/jokes/random" 
     let response;
