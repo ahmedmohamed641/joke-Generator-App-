ddc0d7b4ccd198b46b29c9d0ebf089d6a429f7b0
Your Name <you@example.com>
2024-05-03 06:34:05 +0300

    first commit before review

---
+++ javascript/loader.js
@@ -0,0 +1,14 @@
+const loaderContainer = document.querySelector('.loader-container');
+
+const displayLoading = () => {
+    loaderContainer.style.display = 'block';
+};
+
+const hideLoading = () => {
+    loaderContainer.style.display = 'none';
+};
+
+export {displayLoading};
+export {hideLoading}
+
+
